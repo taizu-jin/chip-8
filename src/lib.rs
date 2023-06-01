faf72ab39bf72c39d86241ce3ef2be5f61edbdc5
taizu-jin <mantas.scc@gmail.com>
2023-06-01 15:56:58 +0300

    feat: CALL addr RET Op codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+use core::panic;
 use std::todo;
 
 pub struct CPU {
     program_counter: usize,
     pub registers: [u8; 16],
     pub memory: [u8; 0x1000],
+    stack: [u16; 32],
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -11,16 +14,17 @@ impl CPU {
 
     pub fn new() -> Self {
         CPU {
-            program_counter: 0,
+            program_counter: Self::SYSTEM_OFFEST as usize,
             registers: [0; 16],
             memory: [0; 4096],
+            stack: [0; 32],
+            stack_pointer: 0,
         }
     }
 
     fn read_opcode(&self) -> u16 {
-        let p = self.program_counter + Self::SYSTEM_OFFEST as usize;
-        let op_byte1 = self.memory[p] as u16;
-        let op_byte2 = self.memory[p + 1] as u16;
+        let op_byte1 = self.memory[self.program_counter] as u16;
+        let op_byte2 = self.memory[self.program_counter + 1] as u16;
 
         op_byte1 << 8 | op_byte2
     }
@@ -28,33 +32,72 @@ impl CPU {
     pub fn run(&mut self) {
         loop {
             let opcode = self.read_opcode();
-            self.program_counter += 2;
 
-            let c = ((opcode & 0xF000) >> 12) as u8;
+            let op_subgroup = (opcode & 0x000F) as u8;
+
             let x = ((opcode & 0x0F00) >> 8) as u8;
             let y = ((opcode & 0x00F0) >> 4) as u8;
-            let d = (opcode & 0x000F) as u8;
 
-            match (c, x, y, d) {
-                (0, 0, 0, 0) => return,
-                (0x8, _, _, 0x4) => self.add_xy(x, y),
+            let nnn = opcode & 0x0FFF;
+
+            self.program_counter += 2;
+
+            match opcode {
+                0x0000 => return,
+                0x00EE => self.ret(),
+                0x2000..=0x2FFF => self.call(nnn),
+                0x8000..=0x8FFF => match op_subgroup {
+                    4 => self.add_xy(x, y),
+                    _ => todo!("opcode {:04x}", opcode),
+                },
                 _ => todo!("opcode {:04x}", opcode),
             }
         }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        let arg1 = self.registers[x as usize];
-        let arg2 = self.registers[y as usize];
+        let x_ = self.registers[x as usize];
+        let y_ = self.registers[y as usize];
 
-        let (val, overflow) = arg1.overflowing_add(arg2);
+        let (val, is_overflown) = x_.overflowing_add(y_);
         self.registers[x as usize] = val;
 
-        if overflow {
+        if is_overflown {
             self.registers[0xF] = 1;
         } else {
             self.registers[0xF] = 0;
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        if addr < Self::SYSTEM_OFFEST {
+            panic!("Trying to access memory reserved for the system!")
+        }
+
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stach overflow!")
+        }
+
+        stack[sp] = self.program_counter as u16;
+        self.stack_pointer += 1;
+        self.program_counter = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+        let call_addr = self.stack[self.stack_pointer];
+        self.program_counter = call_addr as usize;
+    }
+
+    pub fn reset(&mut self) {
+        *self = Self::new();
     }
 }
 
@@ -97,5 +140,36 @@ mod tests {
         cpu.run();
 
         assert_eq!(cpu.registers[0], 35);
+    }
+
+    #[test]
+    fn call_and_return() {
+        let mut cpu = CPU::new();
+
+        cpu.registers[0] = 5;
+        cpu.registers[1] = 10;
+
+        let mem = &mut cpu.memory;
+
+        // Add twice function at address 0x400
+        mem[0x400] = 0x80;
+        mem[0x401] = 0x14;
+        mem[0x402] = 0x80;
+        mem[0x403] = 0x14;
+        mem[0x404] = 0x00;
+        mem[0x405] = 0xEE;
+
+        // Call function twice
+        mem[0x200] = 0x24;
+        mem[0x201] = 0x00;
+        mem[0x202] = 0x24;
+        mem[0x203] = 0x00;
+        // End program
+        mem[0x204] = 0x00;
+        mem[0x205] = 0x00;
+
+        cpu.run();
+
+        assert_eq!(cpu.registers[0], 45);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,32 @@ fn main() {
     cpu.run();
 
     println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+
+    cpu.reset();
+
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    let mem = &mut cpu.memory;
+
+    // Add twice function at address 0x400
+    mem[0x400] = 0x80;
+    mem[0x401] = 0x14;
+    mem[0x402] = 0x80;
+    mem[0x403] = 0x14;
+    mem[0x404] = 0x00;
+    mem[0x405] = 0xEE;
+
+    // Call function twice
+    mem[0x200] = 0x24;
+    mem[0x201] = 0x00;
+    mem[0x202] = 0x24;
+    mem[0x203] = 0x00;
+    // End program
+    mem[0x204] = 0x00;
+    mem[0x205] = 0x00;
+
+    cpu.run();
+
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
